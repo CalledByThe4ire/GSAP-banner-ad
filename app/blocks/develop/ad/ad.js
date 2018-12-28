8d34caa572a58a196309cb2651f84eb9b557e212
Andrey Karamnoff <karamnoff@gmail.com>
2018-12-28 23:38:26 +0300

    add helper function for headings' rendering

--- app/blocks/develop/ad/ad.js
+++ app/blocks/develop/ad/ad.js
@@ -1,232 +1,50 @@
 /* eslint-env browser */
-// eslint-disable-next-line
 const ad = document.querySelector('.ad');
-const heading = document.querySelector('.heading');
-// eslint-disable-next-line
+const letter = document.querySelectorAll('.heading__letter');
+
+const randomNumber = limit => Math.floor(Math.random() * limit) + 1;
+
+const randomizeLetters = (
+  instance,
+  array,
+  speed = 2,
+  i = 10,
+  j = 360,
+  { top = 210, left = -70, rotation = 0, ease = Power2.easeOut } = {},
+  delay = 'letter-=0.9'
+) => {
+  let l = left;
+  array.forEach(item => {
+    instance.to(
+      item,
+      speed,
+      {
+        top,
+        left: l >= 0 ? (l += i) : (l += i),
+        rotation: randomNumber(j),
+        ease
+      },
+      delay
+    );
+  });
+  return instance;
+};
+
 const tl1 = new TimelineMax();
 tl1
   .to(ad, 0.5, { opacity: 1 })
   .from(ad.querySelector('.ad__heading--2nd'), 1.3, {
     top: -100,
-    // eslint-disable-next-line
     ease: Power2.easeOut
-  })
-  .to(
-    heading.querySelector('.heading__letter--1'),
-    2,
-    {
-      top: 210,
-      left: -70,
-      rotation: -100,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--2'),
-    2,
-    {
-      top: 210,
-      left: -60,
-      rotation: 45,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--3'),
-    2,
-    {
-      top: 210,
-      left: -50,
-      rotation: 200,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--4'),
-    2,
-    {
-      top: 210,
-      left: -40,
-      rotation: -130,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--5'),
-    2,
-    {
-      top: 210,
-      left: -30,
-      rotation: 140,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--6'),
-    2,
-    {
-      top: 210,
-      left: -20,
-      rotation: 125,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--7'),
-    2,
-    {
-      top: 210,
-      left: -10,
-      rotation: 95,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--8'),
-    2,
-    {
-      top: 210,
-      left: 0,
-      rotation: -80,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--9'),
-    2,
-    {
-      top: 210,
-      left: 10,
-      rotation: 220,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--10'),
-    2,
-    {
-      top: 210,
-      left: 20,
-      rotation: -310,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--11'),
-    2,
-    {
-      top: 210,
-      left: 30,
-      rotation: 48,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--12'),
-    2,
-    {
-      top: 210,
-      left: 40,
-      rotation: -30,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--13'),
-    2,
-    {
-      top: 210,
-      left: 50,
-      rotation: -67,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--14'),
-    2,
-    {
-      top: 210,
-      left: 60,
-      rotation: -83,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--15'),
-    2,
-    {
-      top: 210,
-      left: 70,
-      rotation: 70,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--16'),
-    2,
-    {
-      top: 210,
-      left: 80,
-      rotation: 280,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  .to(
-    heading.querySelector('.heading__letter--17'),
-    2,
-    {
-      top: 210,
-      left: 90,
-      rotation: -100,
-      // eslint-disable-next-line
-      ease: Power2.easeOut
-    },
-    'letter-=0.9'
-  )
-  // .to(document.querySelector('.ad__heading--1st'), 1, {
-  //   top: 210,
-  //   // eslint-disable-next-line
-  //   ease: Power2.easeOut,
-  //   delay: '2'
-  // })
+  });
+setHeadings(tl1, Array.from(letter));
+
+tl1
   .from(
     ad.querySelector('.ad__img--marker'),
     1,
     {
       top: -110,
-      // eslint-disable-next-line
       ease: Bounce.easeOut
     },
     '-=1'
@@ -236,7 +54,6 @@ tl1
     1,
     {
       opacity: 0,
-      // eslint-disable-next-line
       ease: Bounce.easeOut
     },
     '-=1'
@@ -247,7 +64,6 @@ tl1
     {
       opacity: 0,
       scale: 0.2,
-      // eslint-disable-next-line
       ease: Power2.easeOut
     },
     '-=0.25'
@@ -259,7 +75,6 @@ tl1
       top: 260,
       left: 300,
       rotation: -130,
-      // eslint-disable-next-line
       ease: Back.easeOut
     },
     'cta'
@@ -267,10 +82,8 @@ tl1
 
 // tl1.seek('cta');
 
-// eslint-disable-next-line
 const tl2 = new TimelineMax({ repeat: -1 });
 tl2.to(ad.querySelector('.ad__img--clouds'), 65, {
   backgroundPositionX: 649,
-  // eslint-disable-next-line
   ease: Power0.easeOut
 });
